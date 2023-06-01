088c40416fa152b3eb53448f331f217ebcea833e
Danny <dd8611706@gmail.com>
2023-06-01 22:18:23 +0800

    first commit

---
+++ cogs/help.py
@@ -0,0 +1,14 @@
+import discord
+from core.classes import Cog_Extension
+from discord import app_commands
+
+class Help(Cog_Extension):
+    @app_commands.command(name = "help", description = "Show how to use")
+    async def help(self, interaction: discord.Interaction):
+        embed=discord.Embed(title="Help", description="[see more](https://github.com/FuseFairy/DiscordBot-BardGPT/blob/main/requirements.txt)\n\n**COMMANDS -**")
+        embed.add_field(name="/bard_cookies", value="Set and delete your Google Bard Cookies.", inline=False)
+        embed.add_field(name="/bard", value="Chat with Google Bard.", inline=False)
+        await interaction.response.send_message(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Help(bot))
