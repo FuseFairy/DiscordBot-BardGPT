b5a768d74019e618582d18dfcd44655bd1d4723d
Danny <dd8611706@gmail.com>
2023-07-19 18:33:18 +0800

    feat: image processing

--- src/response.py
+++ src/response.py
@@ -3,6 +3,7 @@ import asyncio
 import re
 from bardapi import Bard
 from src import log
+from typing import Optional
 
 logger = log.setup_logger(__name__)
 using_func = {}
@@ -13,12 +14,15 @@ async def get_using_send(user_id):
 async def set_using_send(user_id, status: bool):
     using_func[user_id] = status
 
-async def chatbot_ask(chatbot: Bard, message: str):
+async def chatbot_ask(chatbot: Bard, message: str, image=None):
     loop = asyncio.get_running_loop()
-    result = await loop.run_in_executor(None, chatbot.get_answer, message)
+    if image:
+        result = await loop.run_in_executor(None, chatbot.ask_about_image, message, image)
+    else:
+        result = await loop.run_in_executor(None, chatbot.get_answer, message)
     return result
 
-async def send_message(chatbot: Bard, interaction: discord.Interaction, user_message: str):
+async def send_message(chatbot: Bard, interaction: discord.Interaction, user_message: str, image=None):
     using_func[interaction.user.id] = True
     reply = ''
     text = ''
@@ -27,7 +31,10 @@ async def send_message(chatbot: Bard, interaction: discord.Interaction, user_mes
     more_images_embed = ''
     await interaction.response.defer(ephemeral=False, thinking=True)
     try:
-        reply = await chatbot_ask(chatbot, user_message)
+        if image:
+            reply = await chatbot_ask(chatbot, user_message, image)
+        else:
+            reply = await chatbot_ask(chatbot, user_message)
 
         # Get reply text
         text = f"{reply['content']}"
@@ -47,10 +54,16 @@ async def send_message(chatbot: Bard, interaction: discord.Interaction, user_mes
 
         # Get the image, if available
         try:
-            if reply["images"] != "set()":
+            if ('' not in reply["images"] and len(reply["images"]) > 0) or len(reply["links"]) > 0:
                 i = 1
                 count = 0
-                for image_link in reply["images"]:
+
+                if len(reply["links"]) > 0:
+                    images = reply["links"]
+                else:
+                    images = reply["images"]
+
+                for image_link in images:
                     if len(images_embed) < 10:
                         images_embed.append(discord.Embed(url=f"https://bard.google.com/{i}").set_image(url=image_link))
                         count += 1
@@ -61,11 +74,12 @@ async def send_message(chatbot: Bard, interaction: discord.Interaction, user_mes
                         more_images_links.append(image_link)
                 if len(more_images_links) > 0:
                     link_text = "\n\n".join(more_images_links)
-                    more_images_embed = discord.Embed(title= "More Images", description=link_text)
+                    if len(link_text) < 4096:
+                        more_images_embed = discord.Embed(title= "More Links", description=link_text)
         except:
             pass
         else:
-            if images_embed and more_images_links:
+            if images_embed and more_images_embed:
                 await interaction.followup.send(response, embeds=images_embed, wait=True)
                 await interaction.followup.send(embed=more_images_embed, wait=True)
             elif images_embed:
