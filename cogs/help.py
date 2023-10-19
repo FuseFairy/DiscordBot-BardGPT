1f51675ec13c05a8233d667242a1fec54a903878
FuseFairy <dd8611706@gmal.com>
2023-10-19 20:42:14 +0800

    change link

--- cogs/help.py
+++ cogs/help.py
@@ -5,7 +5,7 @@ from discord import app_commands
 class Help(Cog_Extension):
     @app_commands.command(name = "help", description = "Show how to use command.")
     async def help(self, interaction: discord.Interaction):
-        embed=discord.Embed(title="Help", description="[see more](https://github.com/FuseFairy/DiscordBot-BardGPT/blob/main/requirements.txt)\n\n**COMMANDS -**")
+        embed=discord.Embed(title="Help", description="[see more](https://github.com/FuseFairy/DiscordBot-BardGPT/blob/main/README.md)\n\n**COMMANDS -**")
         embed.add_field(name="/bard_cookies", value="Set and delete your Google Bard Cookies.", inline=False)
         embed.add_field(name="/bard", value="Chat with Google Bard.", inline=False)
         await interaction.response.send_message(embed=embed)
